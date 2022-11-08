b0c8023662f07df6e11f8f267d2a047bd025326f
Eduard Fadieiev <eddy.fadeev@gmail.com>
2022-11-08 16:37:53 -0700

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,52 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {NgIconsModule} from "@ng-icons/core";
+import {
+  tablerBrandLinkedin,
+  tablerBrandFacebook,
+  tablerBrandTelegram,
+  tablerBrandGithub,
+tablerHeart} from '@ng-icons/tabler-icons';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'skills', component: SkillsComponent},
+  { path: 'portfolio', component: PortfolioComponent},
+  { path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    NgIconsModule.withIcons({
+      tablerBrandLinkedin,
+      tablerBrandFacebook,
+      tablerBrandTelegram,
+      tablerBrandGithub,
+      tablerHeart}),
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
